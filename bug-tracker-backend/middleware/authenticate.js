d4bd06839e8cfb362533c96c3a2c972515a3481c
danimohli <danimohlife@gmail.com>
2025-01-03 10:39:20 +0100

    Tested and validated Project CRUD operations

---
+++ bug-tracker-backend/middleware/authenticate.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).json({ error: 'No token provided' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ error: 'Token invalid' });
+
+    req.user = decoded;
+    next();
+  });
+};
